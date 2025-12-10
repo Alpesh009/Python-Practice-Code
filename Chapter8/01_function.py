f8dc1143c23acfda915d8c2be54acb79dcbcddb3
Alpesh Solanki <40392553+Alpesh009@users.noreply.github.com>
2025-12-10 11:43:56 +0530

    Create 01_function.py

---
+++ Chapter8/01_function.py
@@ -0,0 +1,12 @@
+# function defination
+def average():
+    a = int(input("Enter a number: "))
+    b = int(input("Enter a number: "))
+    c = int(input("Enter a number: "))
+
+    average = (a + b + c)/3
+    print(average)
+
+
+# Function call
+average()

4887cec795d7cbb7330b1f6ef5598a390bf9806b
Alpesh Solanki <40392553+Alpesh009@users.noreply.github.com>
2025-12-10 11:32:04 +0530

    Add files via upload

---
+++ Chapter4/02_list_method.py
@@ -0,0 +1,15 @@
+friends = ["Apple","Orange",5,3.45,False,"Akash","Rohan"]
+print(friends)
+friends.append("Alpesh") # Add the end 
+print(friends)
+
+
+l1 = [1, 4, 55, 75, 4, 33]
+# l1.sort()
+# l1.reverse()
+l1.insert(4, 45.78) # Insert on particular index
+l1.pop(2) # Pop the value
+print(l1.pop(2))
+
+l1.remove(4)
+print(l1)
---
+++ Chapter4/03_tupple.py
@@ -0,0 +1,10 @@
+a = (1,77,5,333,5856,87,33)
+print(a)
+print(type(a))
+
+no = a.count(333)
+print(no)
+l = a.index(5)
+print(l)
+
+print(len(a))
